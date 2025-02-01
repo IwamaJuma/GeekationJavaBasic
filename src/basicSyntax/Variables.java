21a62269914af73eac711c39960745b23501c7e2
'iwama' <'takedajuma@gmail.com'>
2025-02-01 16:27:37 +0900

    Variables finished

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,18 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int number1 = 8;
+		double number2 = 2.73;
+		String sentence = "私の好きな";
+		
+		System.out.println(number1);
+		System.out.println(number2);
+		System.out.println(sentence + "整数は" + number1 + "です。");
+		System.out.println(sentence + "小数は" + number2 + "です。");
+		
+
+	}
+
+}
